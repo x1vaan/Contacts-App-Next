a27243ee635b5f9dde303893af1f2ed3dbc9d018
x1vaan <ivanx467@gmail.com>
2023-10-12 19:05:47 -0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Contacts App',
+  description: 'Contacts App is a Web App to save your contacts',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
