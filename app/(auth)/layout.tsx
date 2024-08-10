72e5bc5a21f3b26e0e2a36cd1207af4993c37f34
Ivan Garcia <ivanx467@gmail.com>
2024-08-10 16:12:42 -0300

    changes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,10 +2,10 @@ import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="w-full h-full justify-center items-center flex absolute">
-      <div className="w-[85%] xl:w-[30rem] md:w-[30rem] sm:w-[30rem] h-auto bg-white shadow-2xl flex flex-col items-center rounded-xl">
+    <section className="w-full h-full justify-center items-center flex absolute">
+      <article className="w-[85%] xl:w-[30rem] md:w-[30rem] sm:w-[30rem] h-auto bg-white shadow-2xl flex flex-col items-center rounded">
         {children}
-      </div>
-    </div>
+      </article>
+    </section>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
             href="https://www.linkedin.com/in/sergio-ivan-garcia/"
             target="_blank"
             rel="noopener noreferrer"
+            className="hover:text-customviolet"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -22,6 +23,7 @@ export default function Home() {
               stroke-width="1.5"
               stroke-linecap="round"
               stroke-linejoin="round"
+              className="hover:stroke-customviolet"
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
@@ -46,6 +48,7 @@ export default function Home() {
               stroke-width="1.5"
               stroke-linecap="round"
               stroke-linejoin="round"
+              className="hover:stroke-customviolet"
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
@@ -68,11 +71,14 @@ export default function Home() {
         </div>
       </div>
       <div className="w-full h-full flex justify-center items-center mt-10 gap-4">
-      <Link href="/login" className="flex justify-center item-center w-[30%] absolute">
-        <Button className="bg-neutral-800 hover:text-customviolet">
-          Start now <ArrowRight className="ml-2 h-4 w-4"/>
-        </Button>
-      </Link>
+        <Link
+          href="/login"
+          className="flex justify-center item-center w-[30%] absolute"
+        >
+          <Button className="bg-neutral-800 hover:text-customviolet">
+            Start now <ArrowRight className="ml-2 h-4 w-4" />
+          </Button>
+        </Link>
       </div>
     </div>
   );
