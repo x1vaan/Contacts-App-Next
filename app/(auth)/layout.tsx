960c325b00001ecb429339977b2d6df91b5cc9d1
Ivan Garcia <ivanx467@gmail.com>
2024-09-23 16:59:19 -0300

    2 fix zoom iphone

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <section className="w-screen h-screen justify-center items-center flex absolute">
+    <section className="w-screen h-screen justify-center items-center flex absolute overflow-hidden">
       {children}
     </section>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
+  userScalable: false,
 };
 
 export const metadata: Metadata = {
